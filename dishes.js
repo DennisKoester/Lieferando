8833e777f86f7edcc717de63626cd33e58991282
StayAbroad <dennis.koester11@gmail.com>
2022-10-18 23:04:22 +0200

    render dishes and basket, basket sticky

--- dishes.js
+++ dishes.js
@@ -2,25 +2,26 @@ let doner = [
     {
         "name": "Doner Kebab",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "6,50"
+        "price": 6.50,
+        "image": "img/doner.jpg"
     },
 
     {
         "name": "Doner Baba",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "7,50"
+        "price": 7.50
     },
 
     {
         "name": "Dürüm Doner",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "7,50"
+        "price": 7.50
     },
 
     {
         "name": "Doner Plate",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "9,90"
+        "price": 9.90
     },
 
 ];
@@ -29,29 +30,29 @@ let lahmacun = [
     {
         "name": "Lahmacun Salad",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "6,50"
+        "price": 6.50
     },
 
     {
         "name": "Lahmacun Doner",
         "ingredients": "Cocktailsauce, Joghurtsauce, Hot, Tzatziki, without sauce and more.",
-        "price": "8,00"
+        "price": 8.00
     },
 ];
 
 let pide = [
     {
         "name": "Pide with minced meat and egg",
-        "price": "9,40"
+        "price": 9.40
     },
     {
         "name": "Pide with spinach and shepherd's cheese",
-        "price": "9,40"
+        "price": 9.40
     },
 
     {
         "name": "Pide with sucukt and egg",
-        "price": "9,40"
+        "price": 9.40
     },
 ];
 
@@ -59,25 +60,25 @@ let salad = [
     {
         "name": "Mixed Salad",
         "ingredients": "Cocktail Dressing, Joghurt Dressing, Vinegar Oil Dressing or without Dressing",
-        "price": "7,50"
+        "price": 7.50
     },
 
     {
         "name": "Hawaiian Salad",
         "ingredients": "Cocktail Dressing, Joghurt Dressing, Vinegar Oil Dressing or without Dressing",
-        "price": "9,00"
+        "price": 9.00
     },
 
     {
         "name": "Italian Salad",
         "ingredients": "Cocktail Dressing, Joghurt Dressing, Vinegar Oil Dressing or without Dressing",
-        "price": "9,00"
+        "price": 9.00
     },
 
     {
         "name": "Chicken Salad",
         "ingredients": "Cocktail Dressing, Joghurt Dressing, Vinegar Oil Dressing or without Dressing",
-        "price": "9,90"
+        "price": 9.90
     },
 ];
 
@@ -85,18 +86,18 @@ let burger = [
     {
         "name": "Hamburger",
         "ingredients": "XL, 1 x Patty or XXL, 2 X Patty",
-        "price": "5,40"
+        "price": 5.40
     },
 
     {
         "name": "Cheeseburger",
         "ingredients": "XL, 1 x Patty or XXL, 2 X Patty",
-        "price": "5,90"
+        "price": 5.90
     },
 
     {
         "name": "Chickenburger XL",
-        "price": "5,40"
+        "price": 5.40
     },
 ]
 
--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 
 function init() {
     renderAllDishes();
-
+    renderBasket();
 }
+
 
 function renderAllDishes() {
     renderDoner();
     renderLahmacun();
-    /* renderPide();
+    renderPide();
     renderSalad();
-    renderBurger(); */
+    renderBurger();
 }
+
+
+function renderBasket(){
+    basketHTMLTemplate();
+}
+
 
 function renderDoner() {
 
@@ -23,36 +30,69 @@ function renderDoner() {
 }
 
 
-function renderLahmacun(){
+function renderLahmacun() {
 
     for (let i = 0; i < lahmacun.length; i++) {
-        
+
         lahmacunHTMLTemplate(i);
     }
 }
 
 
-function donerHTMLTemplate(i){
+function renderPide() {
+
+    for (let i = 0; i < pide.length; i++) {
+
+        pideHTMLTemplate(i);
+    }
+}
+
+
+function renderSalad() {
+
+    for (let i = 0; i < salad.length; i++) {
+
+        saladHTMLTemplate(i);
+    }
+}
+
+
+function renderBurger() {
+
+    for (let i = 0; i < burger.length; i++) {
+
+        burgerHTMLTemplate(i);
+    }
+}
+
+// Basket
+
+
+
+function donerHTMLTemplate(i) {
     let donerDishes = document.getElementById('doner');
+    let dish = doner[i];
+    let price = doner[i]['price'];
 
     return donerDishes.innerHTML += /*html*/ `
     <div id="dish-container" class="dish-container" onclick="addToBasket()">
         <div class="dish">
             <div class="dish-name">
-                <h4>${doner[i]['name']}</h4>
+                <h4>${dish['name']}</h4>
                 <a href="">Product Info</a>
             </div>
-                <div class="ingridents">${doner[i]['ingredients']}</div>
-                <div class="dish-price">${doner[i]['price']} €</div>
+                <div class="ingridents">${dish['ingredients']}</div>
+                <div class="dish-price">${price.toFixed(2).replace('.', ',')} €</div>
             </div>
             <div class="plus-btn">
                 <img src="img/icons8-plus-48.png" alt="Plus Button">
             </div>
         </div>
     </div>`
+}
 
 
-}function lahmacunHTMLTemplate(i){
+function lahmacunHTMLTemplate(i) {
     let lahmacunDishes = document.getElementById('lahmacun');
 
     return lahmacunDishes.innerHTML += /*html*/ `
@@ -73,8 +113,79 @@ function donerHTMLTemplate(i){
 }
 
 
+function pideHTMLTemplate(i) {
+    let pideDishes = document.getElementById('pide');
+
+    return pideDishes.innerHTML += /*html*/ `
+    <div id="dish-container" class="dish-container" onclick="addToBasket()">
+        <div class="dish">
+            <div class="dish-name">
+                <h4>${pide[i]['name']}</h4>
+                <a href="">Product Info</a>
+            </div>
+                <div class="ingridents">${pide[i]['ingredients']}</div>
+                <div class="dish-price">${pide[i]['price']} €</div>
+            </div>
+            <div class="plus-btn">
+                <img src="img/icons8-plus-48.png" alt="Plus Button">
+            </div>
+        </div>
+    </div>`
+}
 
 
-function addToBasket() {
+function saladHTMLTemplate(i) {
+    let saladDishes = document.getElementById('salad');
 
+    return saladDishes.innerHTML += /*html*/ `
+    <div id="dish-container" class="dish-container" onclick="addToBasket()">
+        <div class="dish">
+            <div class="dish-name">
+                <h4>${salad[i]['name']}</h4>
+                <a href="">Product Info</a>
+            </div>
+                <div class="ingridents">${salad[i]['ingredients']}</div>
+                <div class="dish-price">${salad[i]['price']} €</div>
+            </div>
+            <div class="plus-btn">
+                <img src="img/icons8-plus-48.png" alt="Plus Button">
+            </div>
+        </div>
+    </div>`
+}
+
+
+function burgerHTMLTemplate(i) {
+    let burgerDishes = document.getElementById('burger');
+
+    return burgerDishes.innerHTML += /*html*/ `
+    <div id="dish-container" class="dish-container" onclick="addToBasket()">
+        <div class="dish">
+            <div class="dish-name">
+                <h4>${burger[i]['name']}</h4>
+                <a href="">Product Info</a>
+            </div>
+                <div class="ingridents">${burger[i]['ingredients']}</div>
+                <div class="dish-price">${burger[i]['price']} €</div>
+            </div>
+            <div class="plus-btn">
+                <img src="img/icons8-plus-48.png" alt="Plus Button">
+            </div>
+        </div>
+    </div>`
+}
+
+
+function basketHTMLTemplate() {
+    let basketContent = document.getElementById('basket');
+    basketContent.innerHTML = '';
+
+    return basketContent.innerHTML += /*html*/ `
+    <div id="basket-content" class="basket-content">
+        <h2>Shopping Cart</h2>
+        <img class="basket-logo" src="img/icons8-fast-food-64.png" alt="Logo">
+        <h2>Fill Your Food Cart</h2>
+        <p>Add some tasty dishes from the menu and order your food<p>
+    </div>
+    `
 }
